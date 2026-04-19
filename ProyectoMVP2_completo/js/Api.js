5e79b03d38620336c69455e6360fc02038b57a06
nessappq <angiegofa@unisabana.edu.co>
2026-04-18 19:49:19 -0500

    Update API_URL and improve script cleanup

--- ProyectoMVP2_completo/js/Api.js
+++ ProyectoMVP2_completo/js/Api.js
@@ -1,22 +1,29 @@
-var API_URL = "https://script.google.com/macros/s/AKfycbx65lEM4MDoAphgvOASTVXMmhUBC3eOAmIeEh49z2ALOX3vO4zPFLTNGly2HE7Nc--M/exec";
+var API_URL = "https://script.google.com/macros/s/AKfycbwxo-H_rqE18IT9niqbqWG8IdLzHBVArqVzmTtdBNj1kdtUdiDqa_vWTnTC1DxLo8z9/exec";
 
 var API_DISPONIBLE = true;
 
 function jsonpRequest(url) {
   return new Promise(function(resolve, reject) {
     var callbackName = 'cb_' + Date.now();
+    var script = document.createElement('script');
     window[callbackName] = function(data) {
       resolve(data);
       delete window[callbackName];
-      document.head.removeChild(script);
+      if (script.parentNode) script.parentNode.removeChild(script);
     };
-    var script = document.createElement('script');
     script.src = url + '&callback=' + callbackName;
     script.onerror = function() {
       reject(new Error('Error de conexion'));
       delete window[callbackName];
-      document.head.removeChild(script);
+      if (script.parentNode) script.parentNode.removeChild(script);
     };
+    setTimeout(function() {
+      if (window[callbackName]) {
+        reject(new Error('Timeout'));
+        delete window[callbackName];
+        if (script.parentNode) script.parentNode.removeChild(script);
+      }
+    }, 10000);
     document.head.appendChild(script);
   });
 }
